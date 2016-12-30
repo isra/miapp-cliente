84ae158e429e8ce69dac14368d3f43d79b992377
Isaac Salgado <isra.iiss@gmail.com>
2016-12-30 09:04:38 -0600

    app

---
+++ app/scripts/controllers/movie-add.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieAddCtrl
+ * @description
+ * # MovieAddCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MovieAddCtrl', function ($scope, Movie, $location) {
+
+  	$scope.movie = {};
+  	$scope.saveMovie = function() {
+  		Movie.post($scope.movie).then(function(data, err){
+  			console.log("data", data);
+  			$location.path('/movies');
+  		});
+  	};
+
+  });
